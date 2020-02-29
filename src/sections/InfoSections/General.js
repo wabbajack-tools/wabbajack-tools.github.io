739b775199b86ee38eb2f57da9307ef7bc37abd0
erri120 <erri120@protonmail.com>
2020-02-29 15:56:13 +0100

    Updated general info section

--- src/sections/InfoSections/General.js
+++ src/sections/InfoSections/General.js
@@ -64,53 +64,47 @@ export default function General() {
         Compilation
       </Typography>
       <Typography variant="body1">
-        {/* To compile a Modlist, Wabbajack needs a lot of information and has to
+        To compile a Modlist, Wabbajack needs a lot of information and has to
         know from where every file came from. We start by indexing{' '}
         <b>everything</b>: your game, downloads and installation folder and
         create a <i>VFS cache file</i> containing the location and{' '}
         <Link href="https://github.com/Cyan4973/xxHash">xxHash</Link> of every
         indexed file.
-        <br /> */}
-        To compile a Modlist, Wabbajack needs either an MO2 or Vortex
-        installation. We have separate compilers for both of them but the
-        general procedure is the same: <br />
-        Wabbajack starts by indexing <b>everything</b>: your game, downloads and
-        installation folder. Using information provided by the modlist author,
-        Wabbajack tries to find out where every file came from. We support
-        different sites like Nexus Mods, ModDB, LoversLab, Steam Workshop Items
-        and{' '}
-        <Link href="https://github.com/wabbajack-tools/wabbajack/tree/master/Wabbajack.Lib/Downloaders">
-          more
-        </Link>
-        . After Wabbajack gathered information about every file and found no
-        issues like missing information, it runs every file through a{' '}
-        <i>Compilation Stack</i>.<br />
-        The Compilation Stack is a collection of Steps that try to create a
-        Directive for every file. Taking a look at the{' '}
-        <Link href="https://github.com/wabbajack-tools/wabbajack/blob/master/Wabbajack.Lib/MO2Compiler.cs#L496">
-          MO2 Stack
-        </Link>{' '}
-        we can see that log or cache files get ignored. A list of all
-        Compilation Steps can be found{' '}
-        <Link href="https://github.com/wabbajack-tools/wabbajack/tree/master/Wabbajack.Lib/CompilationSteps">
-          here
-        </Link>
-        .<br />
-        After we ran every file through the stack we look for files that had no
-        match. Compilation is stopped if we find duplicates or missing files.
-        The last steps are about validating the archives to see if they exist
-        and are available to download.
+        <br />
+        After WJ indexed all files and done some pre-validating of the archives,
+        we run every file through a <i>Compilation Stack</i>. This stack
+        contains <i>Compilation Steps</i> that find out from where a file came
+        from. It starts at the top and falls through until one step assigns the
+        file a reason to exist and origin. Some steps simply ignore files like
+        logs, caches or crash dumbs while other deconstruct BSAs, include zEdit
+        patches, patch the game ESMs or re-target paths in config files.
+        <br />
+        The Compilation Stack is the meat of the entire process and will result
+        in a list of directives that will later be saved in the output file. If
+        one file dropped out of the stack it will have no match and the
+        compilation will fail.
+        <br />
+        The last steps during compilation are all about verifying whether or not
+        the archive is still downloadable, if the zEdit merges are valid and if
+        the user can actually install the modlist.
       </Typography>
       {/* INSTALLATION */}
       <Typography variant="subtitle1" style={{ marginTop: '8px' }}>
         Installation
       </Typography>
       <Typography variant="body1">
-        Wabbajack is made to be a one click installer: you start Wabbajack, load
-        the Modlist and hit Install. Like with Compilation, we start by hashing
-        every file. If the user has already downloaded the needed archives than
-        we can skip doing that again, but if they are missing some than
-        Wabbajack will download those archives for them.
+        Wabbajack is made to be a one click installer: You start the
+        application, load the Modlist and hit Install.
+        <br />
+        We start, same as the Compilation, by hashing every file to find out if
+        certain archives are already present. This means that you can close
+        Wabbajack at any time and return to the installation when you got the
+        time as the program will find what needs to be downloaded or installed.
+        <br />
+        Wabbajack can download from a multitude of different websites including,
+        but not limited to, Nexus Mods, GitHub, MEGA, MediaFire, GDrive,
+        Dropbox, AFKMods, BethesdaNet, DeadlyStream, LoversLab, ModDB, Steam
+        Workshop, TESAlliance, VectorPlexus, ENBSeries and more!
         <br />
         After downloading, Wabbajack starts going through all Directives within
         the Modlist and installs the files according to the instructions of
