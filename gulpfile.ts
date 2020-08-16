8af0b92d1606e3254befb20ae67acfc26d0c172e
erri120 <erri120@protonmail.com>
2020-08-16 13:51:56 +0200

    Updated gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { Gulpclass, Task, SequenceTask, MergedTask } from 'gulpclass';
+import { Gulpclass, Task, SequenceTask } from 'gulpclass';
 import axios from 'axios';
 import { src, dest } from 'gulp';
 import _ from 'underscore';
@@ -80,71 +80,3 @@ export class Gulpfile {
     return ['cleanAndDownload', 'copyPublicFiles'];
   }
 }
-
-/*const fs = require('fs');
-const axios = require('axios');
-const _ = require('underscore');
-const { parallel, series, src, dest } = require('gulp');
-const del = require('del');*/
-
-/*function getGitHubDownloadLink(cb: any) {
-  return axios
-    .get('https://api.github.com/repos/wabbajack-tools/wabbajack/releases')
-    .then((res: any) => res.data)
-    .then((data: any) => {
-      if (data) {
-        if (data.length == 0) {
-          cb(new Error(`Data length: ${data.length}`));
-        } else {
-          const assets = data[0].assets;
-          if (assets) {
-            if (assets.length == 0) {
-              cb(new Error(`Assets length: ${assets.length}`));
-            } else {
-              const asset = _.find(assets, (x: any) => {
-                return x.name === 'Wabbajack.exe';
-              });
-              if (asset) {
-                const url = asset.browser_download_url;
-                try {
-                  fs.unlinkSync('.env.local');
-                } catch (e) {}
-                fs.writeFileSync('.env.local', `WABBAJACKEXE=${url}`);
-                console.log(`Found URL: ${url}`);
-                cb();
-              } else {
-                cb(new Error(`Unable to find Wabbajack.exe in ${assets}!`));
-              }
-            }
-          } else {
-            cb(new Error('Assets is undefined!'));
-          }
-        }
-      } else {
-        cb(new Error('Data is undefined!'));
-      }
-    })
-    .catch((err: any) => {
-      console.log(err);
-      cb(new Error(err));
-    });
-}
-
-function cleanDist() {
-  return del('dist/**');
-}
-
-function copyPublicFiles() {
-  return src('./src/public/CNAME')
-    .pipe(src('./src/public/404.html'))
-    .pipe(dest('./dist/'));
-}
-
-exports.clean = cleanDist;
-exports.build = series(
-  parallel(cleanDist, getGitHubDownloadLink),
-  copyPublicFiles
-);
-exports.default = parallel(getGitHubDownloadLink, copyPublicFiles);
-exports.getGitHubDownloadLink = getGitHubDownloadLink;
-*/
