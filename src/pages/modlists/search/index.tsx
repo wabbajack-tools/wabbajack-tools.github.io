347cfdd3512fb6bd8b91543c9182ad8d58de54e2
VenatusSimpleX <venatussimplex@users.noreply.github.com>
2021-04-10 21:12:20 +0800

    Fix modlists.search.all have error at top
    This is because it renders modlists.search as well because exact prop was missing

--- src/pages/modlists/search/index.tsx
+++ src/pages/modlists/search/index.tsx
@@ -8,7 +8,7 @@ import ArchiveSearchPage from './ArchiveSearchPage';
 const SearchPage: React.FC = () => {
   return (
     <React.Fragment>
-      <Route name="modlists.search" component={<ModlistSearchPage />} />
+      <Route exact name="modlists.search" component={<ModlistSearchPage />} />
       <Route
         exact
         name="modlists.search.all"
