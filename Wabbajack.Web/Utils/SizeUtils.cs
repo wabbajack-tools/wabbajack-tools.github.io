332e6dadff78e2e32479c67753814a1c1fa2089b
erri120 <erri120@protonmail.com>
2021-08-16 11:59:15 +0200

    Add Archive Search

---
+++ Wabbajack.Web/Utils/SizeUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Wabbajack.Web.Utils
+{
+    public static class SizeUtils
+    {
+        private static readonly string[] Suffix = {"B", "KB", "MB", "GB", "TB", "PB", "EB"};
+
+        public static string ToFileSizeString(this ulong bytes)
+        {
+            if (bytes == 0) return "0" + Suffix[0];
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return num + Suffix[place];
+        }
+    }
+}
