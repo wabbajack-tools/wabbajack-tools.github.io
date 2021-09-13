38d807b3cb054e025cba69765b9c8bc007b16f37
erri120 <erri120@protonmail.com>
2021-09-13 18:59:08 +0200

    Setup tailwind

---
+++ Wabbajack.Web/tailwind.config.js
@@ -0,0 +1,38 @@
+﻿//const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: [],
+    //darkMode: false, // or 'media' or 'class'
+    darkMode: 'class',
+    theme: {
+        fontFamily: {
+            "title": ["Montserrat"],
+            "subtitle": ["Overpass"]
+        },
+        extend: {
+            screens: {
+                '3xl': '1930px'
+            },
+            colors: {
+                red: {
+                    base: '#df3e3e'
+                },
+                black: {
+                    900: '#000000',
+                    800: '#1d1d1d',
+                    700: '#2c2c2c',
+                    600: '#3b3b3b',
+                    500: '#4a4a4a'
+                },
+                purple: {
+                    light: '#c76efb',
+                    dark: '#a43edf'
+                }
+            }
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
